lyx2014/Asterisk

commit 4761dde297e5268681601e96d2cebf7df76e0d46

Preserve DTMF length in main/features.c
Review: https://reviewboard.asterisk.org/r/1463/

A small part of much larger work with DTMF duration in Asterisk, 
funded  by IPvision AS in Denmark.

Thanks to irroot for the review!



git-svn-id: http://svn.asterisk.org/svn/asterisk/trunk@338623 f38db490-d61c-443f-a65b-d21fe96a405b

main/features.c
@@ -3908,11 +3908,11 @@ int ast_bridge_call(struct ast_channel *chan, struct ast_channel *peer, struct a
 					   digits to come in for features. */
 					ast_debug(1, "Timed out for feature!\n");
 					if (!ast_strlen_zero(peer_featurecode)) {
-						ast_dtmf_stream(chan, peer, peer_featurecode, 0, 0);
+						ast_dtmf_stream(chan, peer, peer_featurecode, 0, f->len);
 						memset(peer_featurecode, 0, sizeof(peer_featurecode));
 					}
 					if (!ast_strlen_zero(chan_featurecode)) {
-						ast_dtmf_stream(peer, chan, chan_featurecode, 0, 0);
+						ast_dtmf_stream(peer, chan, chan_featurecode, 0, f->len);
 						memset(chan_featurecode, 0, sizeof(chan_featurecode));
 					}
 					if (f)
@@ -4039,6 +4039,7 @@ int ast_bridge_call(struct ast_channel *chan, struct ast_channel *peer, struct a
 		} else if (f->frametype == AST_FRAME_DTMF_END) {
 			char *featurecode;
 			int sense;
+			unsigned int dtmfduration = f->len;
 
 			hadfeatures = hasfeatures;
 			/* This cannot overrun because the longest feature is one shorter than our buffer */
@@ -4072,7 +4073,7 @@ int ast_bridge_call(struct ast_channel *chan, struct ast_channel *peer, struct a
 				res = feature_interpret(chan, peer, config, featurecode, sense);
 				switch(res) {
 				case AST_FEATURE_RETURN_PASSDIGITS:
-					ast_dtmf_stream(other, who, featurecode, 0, 0);
+					ast_dtmf_stream(other, who, featurecode, 0, dtmfduration);
 					/* Fall through */
 				case AST_FEATURE_RETURN_SUCCESS:
 					memset(featurecode, 0, sizeof(chan_featurecode));

commit 17e50afe0c65f59c36207659b9c541455bc45525

Fixes a segfault caused by referencing null frames introduced in r338623

main/features.c
@@ -4053,11 +4053,11 @@ int ast_bridge_call(struct ast_channel *chan, struct ast_channel *peer, struct a
 					   digits to come in for features. */
 					ast_debug(1, "Timed out for feature!\n");
 					if (!ast_strlen_zero(peer_featurecode)) {
-						ast_dtmf_stream(chan, peer, peer_featurecode, 0, f->len);
+						ast_dtmf_stream(chan, peer, peer_featurecode, 0, f ? f->len : 0);
 						memset(peer_featurecode, 0, sizeof(peer_featurecode));
 					}
 					if (!ast_strlen_zero(chan_featurecode)) {
-						ast_dtmf_stream(peer, chan, chan_featurecode, 0, f->len);
+						ast_dtmf_stream(peer, chan, chan_featurecode, 0, f ? f->len : 0);
 						memset(chan_featurecode, 0, sizeof(chan_featurecode));
 					}
 					if (f)